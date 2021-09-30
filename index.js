4fb850e284abfc712980870378ed0cbda1267c6c
Eddie <elremolino91@gmail.com>
2021-09-29 19:45:14 -0700

    validated transaction data

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+
+app.post('/transaction', (req, res) => {
+  const {payer, points, timestamp} = req.body;
+
+  if (typeof payer !== 'string' || payer.length === 0) {
+    console.error('transaction error, payer is not a valid string');
+  }
+
+  if (typeof points !== 'number') {
+    console.error('transaction error, points is not a valid number');
+  }
+
+  if ((new Date(timestamp)).getTime() <= 0) {
+    console.error('transaction error, timestamp is not a valid date');
+  }
+
+  res.send(200);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
