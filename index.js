1621c171f6f9e345b5980a0624c79b6eaeab6cb2
Eddie <elremolino91@gmail.com>
2021-10-05 20:14:13 -0700

    added further logic to transaction route. Payer and points updated now

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
 const express = require('express');
+const PriorityQueue = require('./dataStores/PriorityQueue');
+const User = require('./dataStores/User');
+// const helper = require('./helperFunctions.js');
 const app = express();
 const port = 3000;
+const payers = {};
+const transactions = new PriorityQueue();
 
 app.use(express.json());
-
-app.post('/transaction', (req, res) => {
+const test = (req, res, next) => {
+  console.log('test', payers);
+  next();
+}
+app.post('/transaction',test, (req, res) => {
   const {payer, points, timestamp} = req.body;
 
   if (typeof payer !== 'string' || payer.length === 0) {
     console.error('transaction error, payer is not a valid string');
+    res.sendStatus(400);
   }
 
   if (typeof points !== 'number') {
     console.error('transaction error, points is not a valid number');
+    res.sendStatus(400);
   }
 
   if ((new Date(timestamp)).getTime() <= 0) {
     console.error('transaction error, timestamp is not a valid date');
+    res.sendStatus(400);
   }
 
-  res.send(200);
+  if(!payers[payer]) {
+    if (points < 0) {
+      console.error('transaction error, points put payer in defecit');
+      res.status(400);
+    // res.sendStatus(400);
+    } else {
+      payers[payer] = points;
+    }
+  } else {
+    payers[payer] += points;
+
+    if (payers[payer] < 0) {
+      payers[payer] -= points;
+      console.error('transaction error, points put payer in defecit');
+      res.status(400);
+    } 
+  }
+  console.log(payers);
+  res.json(payers);
 });
 
 app.listen(port, () => {
