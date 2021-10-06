832a5b213b81ec694914d6f94ca515d0aeeacccb
Eddie <elremolino91@gmail.com>
2021-10-05 21:05:07 -0700

    post transaction using validate function now

--- index.js
+++ index.js
@@ -11,20 +11,7 @@ app.use(express.json());
 app.post('/transaction', (req, res) => {
   const {payer, points, timestamp} = req.body;
 
-  if (typeof payer !== 'string' || payer.length === 0) {
-    console.error('transaction error, payer is not a valid string');
-    res.sendStatus(400);
-  }
-
-  if (typeof points !== 'number') {
-    console.error('transaction error, points is not a valid number');
-    res.sendStatus(400);
-  }
-
-  if ((new Date(timestamp)).getTime() <= 0) {
-    console.error('transaction error, timestamp is not a valid date');
-    res.sendStatus(400);
-  }
+  validate(payer, points, timestamp);
 
   if(!payers[payer]) {
     if (points < 0) {
