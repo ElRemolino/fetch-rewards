9f91c7bb0d440446224236ef022fabc38ede96b8
Eddie <elremolino91@gmail.com>
2021-10-11 17:03:45 -0700

    setupQueue called on line 11 for testing purposes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { payers, postTransaction, transactions, validate } from './helpers.js';
+import setupQueue from './tests.js';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+// adds a couple of transactions to our transactions queue
+setupQueue();
 
 app.post('/transaction', (req, res) => {
   const {payer, points, timestamp} = req.body;
@@ -16,12 +20,17 @@ app.post('/transaction', (req, res) => {
     error = true;
   };
 
-  res.json(payers);
+  if (error) {
+    res.sendStatus(400);
+  }
+  transactions.insert({ payer, points, timestamp })
+  console.log(transactions)
+  res.json(transactions)
 });
 
 app.get('/spend', (req, res) => {
   console.log('test get route');
-  
+
 })
 
 app.listen(port, () => {
